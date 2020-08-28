e431a0818a346835420b46a298b66eb866a3894a
paramesh28 <44474180+paramesh28@users.noreply.github.com>
2020-08-28 09:46:34 -0400

    Add files via upload

---
+++ Spec.js
@@ -0,0 +1,18 @@
+describe('Protractor baby steps',function() {  
+
+
+it('Open Angular js website',function() {
+
+//write your protractor raw code
+	browser.get("https://angularjs.org/");
+	browser.findElement()
+
+})
+
+//Each it block will be called as a spec
+
+it('close browswer', function() {
+
+// code to close browser
+})
+})
